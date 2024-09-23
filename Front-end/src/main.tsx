2688f572c4830a78b1122aa43028abc736598061
Harshaa1818 <114155862+Harshaa1818@users.noreply.github.com>
2024-09-23 22:31:50 +0530

    implemented error boundary

--- Front-end/src/main.tsx
+++ Front-end/src/main.tsx
@@ -2,13 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ErrorBoundary } from 'react-error-boundary'
+import { useErrorBoundary } from 'react-error-boundary'
 
 
 
+
+const CustomComponent:React.FC = () =>{
+  const { resetBoundary } = useErrorBoundary();
+
+  return(
+    <div>
+      <h1>Something went Wrong!!</h1>
+      <button onClick={resetBoundary}>Try again</button>
+    </div>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ErrorBoundary fallback={<CustomComponent/>}>
+
    <App />
+    </ErrorBoundary>
     
   </React.StrictMode>,
 )
